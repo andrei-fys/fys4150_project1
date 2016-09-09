4e6f820804ad8b690eae8db7436abf2f498b3bbf
andrei-fys <andreiku@fys.uio.no>
2016-09-09 15:38:36 +0200

    Output moved to function

--- threediag.cpp
+++ threediag.cpp
@@ -5,31 +5,36 @@
 
 using namespace std;
 
+void file_writer(char* , double* , double* , int  ); 
+
 int main (int argc, char* argv[])
 {
-	int N, i; //N - number of grid points, i - dummy index
-	double h; // step lengh
 	char *output_filename;
-	ofstream ofile;
+	//iofstream ofile;
+	int N = atof(argv[1]);
 	double *grid_points = new double[N]; 
 	double *analytical_solution = new double[N];
-	double precalc_exp; //precalculated value for 1-exp(-10)
 
-	N = atof(argv[1]);
 	output_filename=argv[2];
 
-	h=1.0/(N+1);
-	precalc_exp=1.0-exp(-10.0);
-	for (i=1; i<N; i++) {
+	double h=1.0/(N+1);
+	double precalc_exp=1.0-exp(-10.0);
+	for (int i=1; i<N; i++) {
 		grid_points[i] =  i*h;
 		analytical_solution[i] = 1.0-precalc_exp*grid_points[i]-exp(-10.0*grid_points[i]);
 	}
 	
-	ofile.open(output_filename);
-	for (i=1; i<N; i++) {
-		ofile << grid_points[i] << "," << analytical_solution[i] <<  endl;
-	}
-	ofile.close();
+	file_writer(output_filename, grid_points, analytical_solution, N);
 
 }
 
+void file_writer(char* filename, double* g_points, double* a_solution, int n ) {
+	ofstream ofile;
+	ofile.open(filename);
+	for (int i=1; i<n; i++) {
+		ofile << g_points[i] << "," << a_solution[i] <<  endl;
+	}
+	ofile.close();
+return;
+
+}
