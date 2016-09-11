96685f631bcea0f90f1264c7bbf1af8608f5994f
andrei-fys <andreiku@fys.uio.no>
2016-09-11 14:27:33 +0200

    deleted debug output, added comments

--- threediag.cpp
+++ threediag.cpp
@@ -13,7 +13,6 @@ int main (int argc, char* argv[])
 	char *output_filename_computed;
 	char *output_filename_calculated;
 	char *output_filename_error;
-	//iofstream ofile;
 	int N = atof(argv[1]);
 	double *grid_points = new double[N]; 
 	double *analytical_solution = new double[N];
@@ -39,33 +38,34 @@ int main (int argc, char* argv[])
 	double *b_tilda = new double[N];
 	double *b_prime_tilda = new double[N];
 	double *V = new double[N];
-
+	
 	for (int i=0;i<=N-2;i++) {
 		a[i]=-1.0;
 		c[i]=-1.0;
 	}
-
 	for (int i=0;i<=N-1;i++) {
 		b[i]=2.0;
 		b_tilda[i]=h_squared_100*exp(-10.0*grid_points[i+1]);
-	}
+	}	// RHS of the equation
 	b[N-1]=2.0;
 	b_prime[0]=b[0];
 	b_prime_tilda[0]=b_tilda[0];
+	// GAUSS BF START
+	// forward subst.
 	for (int j=1;j<=N-1;j++){
 		b_prime[j]=b[j]-a[j-1]*c[j-1]/b_prime[j-1];
 		b_prime_tilda[j]=b_tilda[j]-a[j-1]*b_prime_tilda[j-1]/b_prime[j-1];
-		//cout << "b_prime_tilda " << b_prime_tilda[j] ;
-		//cout << "b_prime " << b_prime[j] << endl;
-
 	}
+	// end of forward subst.
+	
+	// backward subst.
 	V[N-1]=b_prime_tilda[N-1]/b_prime[N-1];
-	//cout << "#########" << V[N-1] << endl;
 	for (int k=N-2;k>=0;k--) {
 		V[k]=(b_prime_tilda[k+1]-c[k]*V[k+1])/b_prime[k];
-		//cout << "k  " << k << endl;
-		//cout << "V  " << V[k] << endl;
 	}
+	// end of backward subst.
+	// GAUSS BF END
+	
 	file_writer(output_filename_computed, grid_points, V, N);
 
 	double *relative_error = new double[N];
